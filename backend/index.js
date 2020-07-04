b3ebe958dfca02cec4884db049d3e8cd868b5251
Melisse Cabral <melissecabral@gmail.com>
2020-07-04 14:21:06 -0300

    add dockerfile

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,9 @@ const mongoose = require("mongoose");
 const cors = require('cors');
 const routes = require("./src/routes");
 
+const PORT = 3000;
+const HOST = '0.0.0.0'
+
 mongoose.connect(
   "mongodb://radarDev:admin@learning-shard-00-00-ehibe.mongodb.net:27017,learning-shard-00-01-ehibe.mongodb.net:27017,learning-shard-00-02-ehibe.mongodb.net:27017/test?ssl=true&replicaSet=Learning-shard-0&authSource=admin&retryWrites=true&w=majority",
   {
@@ -13,8 +16,12 @@ mongoose.connect(
 
 const app = express();
 
+app.get('/', (req, res) => {
+  res.send('Hello World');
+})
+
 app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333);
+app.listen(PORT, HOST);
