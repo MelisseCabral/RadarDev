f8a2c3f261630f835d373aa7f19546e4f75ccb3a
Melisse Cabral <melissecabral@gmail.com>
2020-07-19 17:46:52 -0300

    push

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const cors = require('cors');
+const cors = require("cors");
 const routes = require("./src/routes");
 
-const PORT = 3000;
-const HOST = '0.0.0.0'
+const PORT = 3333;
+const HOST = "0.0.0.0";
 
 mongoose.connect(
   "mongodb://radarDev:admin@learning-shard-00-00-ehibe.mongodb.net:27017,learning-shard-00-01-ehibe.mongodb.net:27017,learning-shard-00-02-ehibe.mongodb.net:27017/test?ssl=true&replicaSet=Learning-shard-0&authSource=admin&retryWrites=true&w=majority",
@@ -16,9 +16,9 @@ mongoose.connect(
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World');
-})
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
 
 app.use(cors());
 app.use(express.json());
